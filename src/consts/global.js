dd32855cd600cd75391f16271f7a09838953cd13
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-07-14 16:54:49 +0100

    Update global.js

--- src/consts/global.js
+++ src/consts/global.js
@@ -6,7 +6,7 @@ const PLATFORM_VERSION_NO = '1.7';
 const PLATFORM_VERSION = 'v' + PLATFORM_VERSION_NO;
 const PLATFORM_VERSION_LONG = 'Version: ' + PLATFORM_VERSION_NO;
 
-const TT_BUILD_ID = 'TYJ3H1Ny1rxHmQnvR18Wg';
+const TT_BUILD_ID = '-3ogD5FwYdxboe6rondyh';
 
 const LATEST_NEWS = {
   0: {
